1170727af59f4a41fc2f6970757b03d7dfac5e54
GillesSivry <sivryclic@gmail.com>
2021-01-20 15:42:14 +0100

    URL_API_distante

--- js/panier.js
+++ js/panier.js
@@ -264,7 +264,7 @@ extractId();
   
   // URL API en ligne = https://api-oc5.herokuapp.com/api/furniture/order 
 
-  post("http://localhost:3000/api/furniture/order", jsonData);   
+  post("https://api-oc5.herokuapp.com/api/furniture/order", jsonData);   
 
     //window.location.href = "confirm.html";
 
